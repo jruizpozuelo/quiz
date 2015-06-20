94c0566cac0b9dc210af34bb45309f1206cd99b2
jruizpozuelo <jruizpozuelo@gmail.com>
2015-06-20 15:45:58 +0200

    Ejercicio obligatorio tema 7 sqlite antes heroku parte3 Autoload

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
-/* GET home page. */
+// GET home page. 
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-/*Definición de rutas de /quizes*/
+
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+//Definición de rutas de /quizes
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
