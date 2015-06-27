4ae86cfe4a074c9b0d25ab8b3aea9cdee41b0307
jruizpozuelo <jruizpozuelo@gmail.com>
2015-06-27 13:47:17 +0200

    Tema 7 validacion

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 // GET home page. 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz' , errors: []});
 });
 
 
@@ -14,6 +14,8 @@ router.param('quizId', quizController.load);  // autoload :quizId
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 router.get('/author', function(req, res) {
 res.render('author');
